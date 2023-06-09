7edd2ccc94bbca365f6922da6885c96d23ca71b2
Hailey Lynch <lynchhg2@gmail.com>
2023-06-09 13:34:38 -0400

    Updates to documentation

--- molecool/__init__.py
+++ molecool/__init__.py
@@ -1,5 +1,6 @@
-"""A Python package to visualize molecules given their Cartesian coordinates"""
-
+"""A Python package to visualize molecules given their Cartesian coordinates
+This was created for the Python Best Practices Workshop
+"""
 # Add imports here
 from .functions import canvas
 
